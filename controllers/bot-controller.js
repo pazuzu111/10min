9837902bc36b3040cf72709335b8512af1e3a977
abrian rodriguez <pazuzublue111@gmail.com>
2017-11-01 22:29:07 -0400

    begin

--- controllers/bot-controller.js
+++ controllers/bot-controller.js
@@ -1,51 +1,51 @@
 
-//import model
-const Messages = require('../models/Bot');
+// //import model
+// const Messages = require('../models/Bot');
 
-//declare ocntroller object
-const botController = {};
+// //declare ocntroller object
+// const botController = {};
 
 
-//controller calls findAll method
-//and renders returned data to admin.ejs
-botController.index = (req, res) => {
-    Messages.findAll()
-    .then(messages => {
-        res.render('admin', {messages});
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({error: err});
-    });
-};
+// //controller calls findAll method
+// //and renders returned data to admin.ejs
+// botController.index = (req, res) => {
+//     Messages.findAll()
+//     .then(messages => {
+//         res.render('admin', {messages});
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         res.status(500).json({error: err});
+//     });
+// };
 
-//controller calls update method and sends alert
-botController.update = (req, res) => {
-    Messages.update({
-        name: req.body.name,
-        message: req.body.message,
-        id:req.params.id})
-    .then(message => {
-        alert('updated!!!')
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({error: err});
-    });
-};
+// //controller calls update method and sends alert
+// botController.update = (req, res) => {
+//     Messages.update({
+//         name: req.body.name,
+//         message: req.body.message,
+//         id:req.params.id})
+//     .then(message => {
+//         alert('updated!!!')
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         res.status(500).json({error: err});
+//     });
+// };
 
-//controller calls destroy method and sends alert
-botController.delete = (req, res) => {
-    Messages.destroy(req.params.id)
-    .then(() => {
-        alert('deleted!!!')
-    })
-    .catch(err => {
-        res.status(500).json({
-        err,
-        });
-    });
-};
+// //controller calls destroy method and sends alert
+// botController.delete = (req, res) => {
+//     Messages.destroy(req.params.id)
+//     .then(() => {
+//         alert('deleted!!!')
+//     })
+//     .catch(err => {
+//         res.status(500).json({
+//         err,
+//         });
+//     });
+// };
 
-//export to use in other files
-module.exports = botController;
+// //export to use in other files
+// module.exports = botController;
--- routes/bot-routes.js
+++ routes/bot-routes.js
@@ -1,18 +1,18 @@
-//requires
-const express = require('express');
-const chatRouter = express.Router();
+// //requires
+// const express = require('express');
+// const chatRouter = express.Router();
 
-//import controller
-const botController = require('../controllers/bot-controller');
+// //import controller
+// const botController = require('../controllers/bot-controller');
 
-//the url that we want the index controller to render data to
-chatRouter.get('/admin', botController.index);
+// //the url that we want the index controller to render data to
+// chatRouter.get('/admin', botController.index);
 
-//the url that we want the update controller to render data to
-chatRouter.put('/admin/:id', botController.update);
+// //the url that we want the update controller to render data to
+// chatRouter.put('/admin/:id', botController.update);
 
-//the url that we want the delete controller to render data to
-chatRouter.delete('/admin/:id', botController.delete);
+// //the url that we want the delete controller to render data to
+// chatRouter.delete('/admin/:id', botController.delete);
 
-//export for use in other files
-module.exports = chatRouter;
+// //export for use in other files
+// module.exports = chatRouter;
