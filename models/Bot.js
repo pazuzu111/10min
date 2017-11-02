c6910604894e833ee52e21f0f3f565bddd8bc249
abrian rodriguez <pazuzublue111@gmail.com>
2017-11-01 22:34:37 -0400

    completed config.js - connected db, completed models

--- models/Bot.js
+++ models/Bot.js
@@ -1,31 +1,31 @@
-// //import db
-// const db = require('../db/config');
+//import db
+const db = require('../db/config');
 
-// //declare model object
-// const Messages = {};
-// // const Messages2 = {};
+//declare model object
+const Messages = {};
+// const Messages2 = {};
 
-// //db query command to find all data in table
-// Messages.findAll = () => {
-//   return db.query(`SELECT * FROM bot1 ORDER BY id ASC`);
-// }
+//db query command to find all data in table
+Messages.findAll = () => {
+  return db.query(`SELECT * FROM bot1 ORDER BY id ASC`);
+}
 
-// //db query command to update data selected
-// Messages.update = (bot) => {
-//   return db.one(
-//   `UPDATE bot1 SET
-//     name = $1,
-//     message = $2
-//     WHERE id = $3
-//     RETURNING *`, [bot.name, bot.message, bot.id]);
-// };
+//db query command to update data selected
+Messages.update = (bot) => {
+  return db.one(
+  `UPDATE bot1 SET
+    name = $1,
+    message = $2
+    WHERE id = $3
+    RETURNING *`, [bot.name, bot.message, bot.id]);
+};
 
-// //db query command to delete data selected
-// Messages.destroy = id => {
-//   return db.none(
-//     `DELETE FROM bot1
-//     WHERE id = $1`, [id]);
-// };
+//db query command to delete data selected
+Messages.destroy = id => {
+  return db.none(
+    `DELETE FROM bot1
+    WHERE id = $1`, [id]);
+};
 
-// //export to use in other files
-// module.exports = Messages;
+//export to use in other files
+module.exports = Messages;
